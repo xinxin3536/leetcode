33f57c457319aad041ac2c97a71aabe3808a445b
xinxin3536 <79047016+xinxin3536@users.noreply.github.com>
2021-02-16 16:35:29 +0800

    Update 27. Remove Element.py

--- 27. Remove Element.py
+++ 27. Remove Element.py
@@ -1,10 +1,8 @@
 class Solution:
     def removeElement(self, nums: List[int], val: int) -> int:
-        tmp = len(nums)
         cnt = 0
         for i in range(0,len(nums)):
-            if nums[i] == val:
-                nums[i] = 0
+            if nums[i] != val:
+                nums[cnt] = nums[i]
                 cnt += 1
-        nums.sort(reverse = True)
-        return len(nums)-cnt
+        return cnt
